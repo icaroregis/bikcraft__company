fcb2c018ee930926b59437b008f8c06696ee6cfe
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-04-18 17:01:18 -0300

    ...

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ let active = true;
 const container__menu__hamburguer = document.querySelector(
   '.container__menu__hamburguer',
 );
-
 const menu__hamburguer = document.querySelector('.menu__hamburguer');
 
 menu__hamburguer.addEventListener('click', () => {
