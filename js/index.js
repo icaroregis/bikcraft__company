dbf03a0e6775f58430fefe34ac2778fa48f251eb
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-04-23 23:52:52 -0300

    finalização de javascript

--- js/index.js
+++ js/index.js
@@ -35,3 +35,34 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', (event) => {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('active');
+    const ativa = resposta.classList.contains('ativa');
+    pergunta.setAttribute('aria-expanded', ativa);
+  });
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//galeria de bicicletas
+const galeria = document.querySelectorAll('.bicicleta__imagem img');
+const galeriaContainer = document.querySelector('.bicicleta__imagem');
+
+function eventosGaleria(img) {
+  img.addEventListener('click', (event) => {
+    const img = event.currentTarget;
+    const media = window.matchMedia('(min-width: 1000px)').matches;
+    media ? galeriaContainer.prepend(img) : '';
+  });
+}
+
+galeria.forEach(eventosGaleria);
